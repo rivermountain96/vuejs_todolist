45dc6d129bfc11e2d498872ffa40b4c92e191206
rivermountain96 <dlrkdtks7@gmail.com>
2023-10-26 14:43:50 +0900

    [ADD] delete 버튼 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <script>
+import Todo from "./components/Todo.vue";
 export default {
+  components: {
+    Todo,
+  },
   data() {
     return {
       todoText: "",
       todos: [
         { id: 1, text: "Learn web", checked: false },
         { id: 2, text: "Get a job", checked: false },
+        { id: 3, text: "Study Hard", checked: false },
       ],
     };
+  },
+  methods: {
+    AddTodo(e) {
+      // console.log(e.target.value);
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = "";
+    },
+    toggleCheck({ id, checked }) {
+      const index = this.todos.findIndex((todo) => todo.id === id);
+      this.todos[index].checked = checked;
+      console.log(this.todos);
+    },
+    deleteTodo(id) {
+      if (window.confirm("정말 삭제할까요")) {
+        const index = this.todos.findIndex((todo) => todo.id === id);
+        this.todos.splice(index, 1);
+      } else {
+        alert("취소되었습니다.");
+      }
+    },
   },
 };
 </script>
@@ -21,8 +50,17 @@ export default {
         type="text"
         class="form-control"
         id="todo"
-        placeholder="할일을 입력하세요" />
+        placeholder="할일을 입력하세요"
+        required
+        @keyup.enter="AddTodo"
+        v-model="todoText" />
     </div>
+    <Todo
+      v-for="todo in todos"
+      key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheck"
+      @click-delete="deleteTodo" />
   </div>
 </template>
 
---
+++ src/components/Todo.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    toggleCheckbox(e) {
+      this.$emit("toggle-checkbox", {
+        id: this.todo.id,
+        checked: e.target.checked,
+      });
+    },
+    clickDelete() {
+      this.$emit("click-delete", this.todo.id);
+    },
+  },
+};
+</script>
+<template>
+  <div class="form-check" :data-id="todo.id">
+    <input
+      class="form-check-input"
+      type="checkbox"
+      value=""
+      :id="'input' + todo.id"
+      @change="toggleCheckbox" />
+    <label
+      class="form-check-label"
+      :for="`input${todo.id}`"
+      :style="{
+        'text-decoration': todo.checked ? 'line-through' : 'none',
+        color: todo.checked ? 'red' : 'black',
+      }">
+      {{ todo.text }}
+    </label>
+    <button
+      type="button"
+      class="btn btn-danger btn-sm ms-2"
+      @click="clickDelete">
+      X
+    </button>
+  </div>
+</template>
